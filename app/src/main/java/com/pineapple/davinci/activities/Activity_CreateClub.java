e0ad99f85c8d703816072e1bb186a08d86d4b655
Gupta <muditg317@gmail.com>
2018-08-27 21:41:31 -0400

    added create club activity -gupta

---
+++ app/src/main/java/com/pineapple/davinci/activities/Activity_CreateClub.java
@@ -0,0 +1,13 @@
+package com.pineapple.davinci.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_CreateClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_club);
+    }
+}
