309701c8d8fbfec1344e405bc7799b1625be5c1c
Gupta <muditg317@gmail.com>
2018-08-27 22:06:28 -0400

    update google-services.json -gupta

--- app/src/main/java/com/pineapple/davinci/activities/Activity_CreateClub.java
+++ app/src/main/java/com/pineapple/davinci/activities/Activity_CreateClub.java
@@ -3,6 +3,8 @@ package com.pineapple.davinci.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pineapple.davinci.R;
+
 public class Activity_CreateClub extends AppCompatActivity {
 
     @Override
