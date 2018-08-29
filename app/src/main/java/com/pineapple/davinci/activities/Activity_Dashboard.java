e9e0cf02902b43e48ae6e9da7e3d12b17cba4e48
Gupta <muditg317@gmail.com>
2018-08-28 21:36:06 -0400

    some more updates on nav bar -gupta

--- app/src/main/java/com/pineapple/davinci/activities/Activity_Dashboard.java
+++ app/src/main/java/com/pineapple/davinci/activities/Activity_Dashboard.java
@@ -31,17 +31,44 @@ public class Activity_Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_dashboard2);
 
-        TextView userID = (TextView) findViewById(R.id.userID);
-        TextView firstName = (TextView) findViewById(R.id.firstName);
-        TextView lastName = (TextView) findViewById(R.id.lastName);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        mStudent = Singleton.getInstance().getCurrStudent();
+                switch(item.getItemId()) {
+                    case R.id.navigation_dashboard:
+                        Intent startIntent1 = new Intent(getApplicationContext(), Activity_Dashboard.class);
+                        startActivity(startIntent1);
+                        break;
+                    case R.id.navigation_calendar:
+                        Intent startIntent2 = new Intent(getApplicationContext(), Activity_Calendar.class);
+                        startActivity(startIntent2);
+                        break;
+                    case R.id.navigation_eclass:
+                        Intent startIntent3 = new Intent(getApplicationContext(), Activity_Eclass.class);
+                        startActivity(startIntent3);
+                        break;
+                    case R.id.navigation_clubs:
+                        Intent startIntent4 = new Intent(getApplicationContext(), Activity_Eclass.class);
+                        startActivity(startIntent4);
+                        break;
+                    case R.id.navigation_Settings:
+                        Intent startIntent5 = new Intent(getApplicationContext(), Activity_Profile.class);
+                        startActivity(startIntent5);
+                        break;
 
-        userID.setText(mStudent.getMyUserID());
-        firstName.setText(mStudent.getFirstName());
-        lastName.setText(mStudent.getLastName());
+                }
+
+                return true;
+            }
+        });
 
 
 
@@ -93,47 +120,5 @@ public class Activity_Dashboard extends AppCompatActivity {
         if(mAuthStateListener != null) {
             FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
         }
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
-        menuItem.setChecked(true);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch(item.getItemId()) {
-                    case R.id.navigation_dashboard:
-                        Intent startIntent1 = new Intent(getApplicationContext(), Activity_Dashboard.class);
-                        startActivity(startIntent1);
-                        break;
-                    case R.id.navigation_calendar:
-                        Intent startIntent2 = new Intent(getApplicationContext(), Activity_Calendar.class);
-                        startActivity(startIntent2);
-                        break;
-                    case R.id.navigation_eclass:
-                        Intent startIntent3 = new Intent(getApplicationContext(), Activity_Eclass.class);
-                        startActivity(startIntent3);
-                        break;
-                    case R.id.navigation_clubs:
-                        Intent startIntent4 = new Intent(getApplicationContext(), Activity_Eclass.class);
-                        startActivity(startIntent4);
-                        break;
-                    case R.id.navigation_Settings:
-                        Intent startIntent5 = new Intent(getApplicationContext(), Activity_Profile.class);
-                        startActivity(startIntent5);
-                        break;
-
-                }
-
-                return true;
-            }
-        });
     }
 }
