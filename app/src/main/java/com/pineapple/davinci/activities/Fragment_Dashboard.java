ebd1eadbe638fdd519e0c6a3dae34a22dd775460
Gupta <muditg317@gmail.com>
2018-08-29 20:44:07 -0400

    set up all of fragment based view flipper stuff -gupta created fragment java and xml files edited AndroidManifest.xml to support changes

---
+++ app/src/main/java/com/pineapple/davinci/activities/Fragment_Dashboard.java
@@ -0,0 +1,187 @@
+package com.pineapple.davinci.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.pineapple.davinci.R;
+import com.pineapple.davinci.resources.Singleton;
+import com.pineapple.davinci.studentutils.Student;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Fragment_Dashboard.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Fragment_Dashboard#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment_Dashboard extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    Button signout;
+
+
+
+    public Fragment_Dashboard() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment_Dashboard.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment_Dashboard newInstance(String param1, String param2) {
+        Fragment_Dashboard fragment = new Fragment_Dashboard();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        TextView userID = (TextView) findViewById(R.id.userID);
+        TextView name = (TextView) findViewById(R.id.name);
+
+        Student student = Singleton.getInstance().getCurrStudent();
+
+        userID.setText(student.getMyUserID());
+        name.setText(student.getFirstName() + " " + student.getLastName());
+
+        setupFirebaseListener();
+
+
+        signout = (Button) findViewById(R.id.button_signout);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("sign i=out", "Onclick: attempting to sign out user");
+                FirebaseAuth.getInstance().signOut();
+                //Auth.GoogleSignInApi.signOut(Singleton.getInstance().getGapiClient());
+                Singleton.getInstance().getGsiClient().signOut();
+            }
+        });
+    }
+
+
+    private void setupFirebaseListener() {
+        Log.d("sign out", "setupFirebaseListener: setting up auth state listener");
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d("sign out", "onAuthStateChanged: signed_in " + user.getUid());
+                }
+                else{
+                    Log.d("sign out", "onAuthStateChanged: signed_out");
+                    Toast.makeText(getActivity().getApplicationContext(), "Signed out", Toast.LENGTH_SHORT).show();
+                    Singleton.getInstance().signOut();
+                    Intent startIntent = new Intent(getActivity().getApplicationContext(), Activity_Login.class);
+                    startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(startIntent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthStateListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    private View findViewById(int id) {
+        return getActivity().findViewById(id);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
